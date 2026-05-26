0703b9639642c54d49182a1ce37b930b8e0beeab
khalid0931 <mutiarif53@gmail.com>
2026-05-26 10:31:55 +0700

    memperbaiki library

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-using namespace namespace std;
+using namespace std;
 
 class Queues
 {
@@ -20,3 +20,10 @@ public:
         int num;
         cout << "Enter a number: ";
         cin >> num;
+        cout << endl;
+
+        // cek apakah antrian penuh
+        if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+        {
+            cout << "\nQueue overflow\n";
+            return;
