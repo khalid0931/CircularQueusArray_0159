98364a8e64f9618e31f146cc7ef3ba924a2f96f4
khalid0931 <mutiarif53@gmail.com>
2026-05-26 10:40:52 +0700

    menyelesaikan void remove

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -37,7 +37,7 @@ public:
         }
         else
         {
-            / jika rear berada di posisi terakhir array, kembali ke awal array
+            // jika rear berada di posisi terakhir array, kembali ke awal array
             if (REAR == max - 1)
                 REAR = 0;
             else
@@ -48,13 +48,29 @@ public:
 
     void remove()
     {
-        // ccek apakah antrian kosong
+        // cek apakah antrian kosong
         if (FRONT == -1)
         {
             cout << "Queue underflow\n";
             return;
         }
         cout << "\nThe element deleted from the queue is: " << queue_array[FRONT] << "\n";
+
+        // cek jika antrian hanya memiliki satu elemen
+        if (FRONT == REAR)
+        {
+            FRONT = -1;
+            REAR = -1;
+        }
+        else
+        {
+            // jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
+        }
+    }
 
     
         
