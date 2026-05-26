0b69c6c4d4748b99db3e1e32e99b861f7c8f0406
khalid0931 <mutiarif53@gmail.com>
2026-05-26 10:36:57 +0700

    menyelesaikan void insert

--- circularqueue.cpp
+++ circularqueue.cpp
@@ -27,3 +27,24 @@ public:
         {
             cout << "\nQueue overflow\n";
             return;
+        }
+
+        //ce apakah antrean kosong
+        if (FRONT == -1)
+        {
+            FRONT = 0;
+            REAR = 0;
+        }
+        else
+        {
+            / jika rear berada di posisi terakhir array, kembali ke awal array
+            if (REAR == max - 1)
+                REAR = 0;
+            else
+                REAR = REAR + 1;
+        }
+        queue_array[REAR] = num;
+    }
+
+    
+        
